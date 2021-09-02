c54a677b4046e19bd6356dc470044560504fd8ad
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-09-02 14:35:35 +0300

    add files

---
+++ js/drum.js
@@ -0,0 +1,19 @@
+window.addEventListener("keydown", onKeydown);
+const keys = document.querySelectorAll(".key");
+
+function onKeydown(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  key.classList.add("playing");
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
